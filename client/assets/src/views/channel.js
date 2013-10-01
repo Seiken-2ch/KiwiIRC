50d9ad19bd1de7e553531b7ac6ab0ff925489587
Seiken-2ch <seiken.public@gmail.com>
2013-10-01 14:47:07 +0800

    Change click behavior and update font
    Click nicknames in chat window no more bring menu but double click will
    insert the nickname in input box automatically.

--- client/assets/src/views/channel.js
+++ client/assets/src/views/channel.js
@@ -6,7 +6,7 @@ _kiwi.view.Channel = _kiwi.view.Panel.extend({
             parent_events = parent_events();
         }
         return _.extend({}, parent_events, {
-            'click .msg .nick' : 'nickClick',
+            'dblclick .msg .nick' : 'nickClick',
             "click .chan": "chanClick",
             'click .media .open': 'mediaClick',
             'mouseenter .msg .nick': 'msgEnter',
@@ -205,41 +205,12 @@ _kiwi.view.Channel = _kiwi.view.Panel.extend({
     nickClick: function (event) {
         var nick = $(event.currentTarget).text(),
             members = this.model.get('members'),
-            member, query, userbox, menubox;
+            member, query;
 
         if (members) {
             member = members.getByNick(nick);
             if (member) {
-                userbox = new _kiwi.view.UserBox();
-                userbox.member = member;
-                userbox.channel = this.model;
-
-                // Hide the op related items if we're not an op
-                if (!members.getByNick(_kiwi.app.connections.active_connection.get('nick')).get('is_op')) {
-                    userbox.$el.children('.if_op').remove();
-                }
-
-                menubox = new _kiwi.view.MenuBox(member.get('nick') || 'User');
-                menubox.addItem('userbox', userbox.$el);
-                menubox.show();
-
-                // Position the userbox + menubox
-                (function() {
-                    var t = event.pageY,
-                        m_bottom = t + menubox.$el.outerHeight(),  // Where the bottom of menu will be
-                        memberlist_bottom = this.$el.parent().offset().top + this.$el.parent().outerHeight();
-
-                    // If the bottom of the userbox is going to be too low.. raise it
-                    if (m_bottom > memberlist_bottom){
-                        t = memberlist_bottom - menubox.$el.outerHeight();
-                    }
-
-                    // Set the new positon
-                    menubox.$el.offset({
-                        left: event.clientX,
-                        top: t
-                    });
-                }).call(this);
+                $(".input_wrap .inp").insertAtCaret(member.get('nick') || 'User');
             }
         }
     },
